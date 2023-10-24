4f95a680c56b6b33e205a0ad07da89c99448f87e
Jee hyun PARK <jeepark@made-f0Dr11s6.clusters.42paris.fr>
2023-10-24 19:00:32 +0200

    doing: display reduced expression

--- src/parsing_utils.py
+++ src/parsing_utils.py
@@ -1,14 +1,33 @@
 import re
+
+
 def split_expression(expression: str) -> str:
-    splitted = expression.split('=', 1)
+    """split the initial expression using '=' sign to
+return left-sided expression and right-sided expression. The expressed get trimmed
+    Args:
+        expression (str): expression describing a polynomial equation
+
+    Raises:
+        ValueError: missing '=' signs meaning is not an equation
+        ValueError: if one sided expression is empty meaning
+        initial expression was incomplete
+
+    Returns:
+        str: left-sided expression and right-sided expression
+    """
+    splitted = expression.split('=')
+    if len(splitted) != 2:
+        raise ValueError('missing or too many equal operator')
+    splitted = [re.sub(r'[\s]', '', term) for term in splitted]
+    if len(splitted[0]) == 0 or len(splitted[1]) == 0:
+        raise ValueError('expression is not complete')
     return splitted[0], splitted[1]
 
+
 def get_coefficients_list(expression: str) -> list[float]:
-    # # split
+    # split
     split_pattern = r'(\+|\-)'
     terms = re.split(split_pattern, expression)
-    # # trim string
-    terms = list(map(lambda term: term.replace(" ", ""), terms))
     terms_with_operator = []
     if terms[0] and terms[0][0].isdigit():
         terms[0] = '+' + terms[0]
@@ -19,10 +38,10 @@ def get_coefficients_list(expression: str) -> list[float]:
         if operator != '':
             if term != "+" and term != '-':
                 terms_with_operator.append(operator + term)
-        if term and term == '+' or term =='-':
+        if term and term == '+' or term == '-':
             operator = term
     coefficients_list = []
-    
+
     for term in terms_with_operator:
         coefficient_end = term.find('*')
         value = float(term[0:coefficient_end])
@@ -30,20 +49,22 @@ def get_coefficients_list(expression: str) -> list[float]:
         if degree >= len(coefficients_list):
             coefficients_list.append([value])
         else:
-            coefficients_list[degree].extend([value])       
+            coefficients_list[degree].extend([value])
     return coefficients_list
+
 
 def calculate_to_reduce(expression: list):
     reduced_list = [[] for x in range(len(expression))]
     res = 0
     index = -1
     for degree in expression:
-        res=0
+        res = 0
         index += 1
         for value in degree:
             res += value
         reduced_list[index].extend([res])
     return reduced_list
+
 
 def create_equation(left: list[float], right: list[float]):
     index = -1
@@ -51,8 +72,17 @@ def create_equation(left: list[float], right: list[float]):
         index += 1
         for value in degree:
             left[index].extend([value])
-    print(left)
-    
+
+
+def negate_expression(expression: list[float]) -> list[float]:
+    i = -1
+    for degree in expression:
+        i += 1
+        j = -1
+        for value in degree:
+            expression[i][j] = value * -1
+            j += 1
+
 
 def reduce_expression(left: list[float], right: list[float]) -> list[float]:
     if len(left) > len(right):
@@ -65,29 +95,30 @@ def reduce_expression(left: list[float], right: list[float]) -> list[float]:
     left_reduced = calculate_to_reduce(left)
     right_reduced = calculate_to_reduce(right)
 
-    index = -1
-    for degree in right_reduced:
-        index += 1
-        jindex = -1
-        for value in degree:
-            
-            right_reduced[index][jindex] =value * -1
-            jindex +=1
-            print(value)
-        # index +=1
+    negate_expression(right_reduced)
 
-    print('after * -1', right_reduced)
+    print('negate right expression:', right_reduced)
     print(f"left reduced: {left_reduced} right reduced: {right_reduced}")
     create_equation(left_reduced, right_reduced)
-    print(f"left reduced: {left_reduced} right reduced: {right_reduced}")
-    
+    print(f"equation: {left_reduced}")
+
     left_reduced = calculate_to_reduce(left_reduced)
-    print(f"left reduced: {left_reduced} right reduced: {right_reduced}")
+    print(f"reduced equation: {left_reduced}")
     return left_reduced
+
 
 def display_reduced_expression(expression: str):
     to_display = ""
     index = -1
-    # for degree in expression:
-    #     index += 1
-    #     for value in degree: 
+    for degree in expression:
+        index += 1
+        for value in degree:
+            print(f"{index} {value}")
+            # if value > 0:
+            #     operator = '+'
+            # else:
+            #     operator = '-'
+            to_display += str(value) + ' * X^' + str(index) + ' '
+
+    print(to_display)
+# Reduced form: 4 * X^0 + 4 * X^1 - 9.3 * X^2 = 0
---
+++ tests/test_parsing.py
@@ -0,0 +1,37 @@
+
+import unittest as ut
+import sys
+import os
+
+from src.parsing_utils import split_expression
+
+
+class TestParsing(ut.TestCase):
+    def test_split_expression(self):
+        self.assertEqual(split_expression(
+            "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"), ("5*X^0+4*X^1-9.3*X^2", "1*X^0"))
+        self.assertEqual(split_expression(
+            "5 * X^0 + 4 * X^1 = 4 * X^0"), ("5*X^0+4*X^1", "4*X^0"))
+
+    def test_invalid_equal_operator(self):
+        with self.assertRaises(ValueError) as cm:
+            split_expression("5 * X^0 + 4 * X^1")
+        self.assertEqual(str(cm.exception),
+                         "missing or too many equal operator")
+        with self.assertRaises(ValueError) as cm:
+            split_expression("5 * = X^0 + 4 = * X = ^1")
+        self.assertEqual(str(cm.exception),
+                         "missing or too many equal operator")
+
+    def test_empty_left_expression(self):
+        with self.assertRaises(ValueError) as cm:
+            split_expression("   = 5 * X^0 + 4 * X^1")
+        self.assertEqual(str(cm.exception), "expression is not complete")
+
+
+if __name__ == "__main__":
+    ut.main()
+
+#  with self.assertRaises(AssertionError) as cm:
+    # Stark("Ned", 123)
+    # self.assertEqual(str(cm.exception), 'is_alive must be bool')
